983449d8d0d653c90aef9908674f5cfe5512e976
SickBow <mikecilona@gmail.com>
2023-04-29 20:28:38 -0400

    Updated to allow for cloning

--- Runtime/Condition.cs
+++ Runtime/Condition.cs
@@ -13,6 +13,10 @@ public class Condition
     [SerializeField] bool isSatisfied;
     [SerializeField] List<ConditionValue> conditionValues;
     
+    public string GetName() => name;
+    public bool GetInvert() => invert;
+    public List<ConditionValue> GetConditionValues() => conditionValues;
+
     public bool GetValue(){
         if (conditionValues.Count == 0) {
             isSatisfied = false;
@@ -27,6 +31,12 @@ public class Condition
         }
         isSatisfied = invert? false : true;
         return invert? false : true;
+    }
+
+    public void Init(string name, bool invert, List<ConditionValue> conditionValues){
+        this.name = name;
+        this.invert = invert;
+        this.conditionValues = conditionValues;
     }
 }
 }
